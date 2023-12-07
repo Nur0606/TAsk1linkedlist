38b5816a80bd71461cf01eb92fcba59ff4564b65
nurhan <141030168+Nur0606@users.noreply.github.com>
2023-12-07 19:30:23 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,89 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+//1
+        //        list.add(2);
+//        list.add(5);
+//        list.add(6);
+//        list.add(8);
+//        System.out.println(list);
+//        Integer integer = list.getFirst();
+//        System.out.println(integer);
+//2
+//        list.add(2);
+//        list.add(5);
+//        list.add(6);
+//        list.add(8);
+//        System.out.println(list);
+//        Integer integer1 = list.getLast();
+//        System.out.println(integer1);
+        //3
+//        Scanner scanner =new Scanner(System.in);
+//        System.out.println("vedi chislo: ");
+//        int num = scanner.nextInt();
+//        list.add(2);
+//        list.add(5);
+//        list.add(6);
+//        list.add(8);
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i)==num){
+//                System.out.println("vasha chislo est");
+//            }else
+//                System.out.println("vasha chislo netu");
+//        }
+//        System.out.println(list);
+//4
+//        Integer[] araay = list.toArray(new Integer[0]);
+//        for (Integer o:araay) {
+//            System.out.println(o);
+        //5
+//        LinkedList<Integer> list1 = new LinkedList<>();
+//        LinkedList<Integer> list2 = new LinkedList<>();
+//
+//        list1.add(5);
+//        list1.add(6);
+//        list1.add(8);
+//        list1.add(2);
+//        System.out.println(list1);
+//        list2.add(5);
+//        list2.add(6);
+//        list2.add(8);
+//        list2.add(1);
+//        list2.add(1);
+//        System.out.println(list2);
+//
+//        for (int i = 0; i < list1.size(); i++) {
+//            if (list1.size() == list2.size()){
+//                System.out.println("true");
+//            }else
+//                System.out.println("false");
+//        }
+//        //6
+//list.add(5);
+//        System.out.println(list);
+//        System.out.println(list.isEmpty());
+
+
+        //7
+//        list.add(34);
+//        list.add(76);
+//        list.add(6);
+//        list.add(3);
+//        System.out.println(list);
+//        Scanner scanner =new Scanner(System.in);
+//        System.out.println("vedi kakuyu hochesh obmenat");
+//        int addchislo = scanner.nextInt();
+//        System.out.println("teper vedi chislo kotoruyu hochesh dobavit");
+//        int newchislo = scanner.nextInt();
+//           list.set(addchislo,newchislo);
+//        System.out.println(list);
+        }
+    }
+
+
+
